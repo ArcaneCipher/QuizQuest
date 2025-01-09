658885088008c70e11a2088292d4e0110df7df53
ArcaneCipher <desjardins.jarp@gmail.com>
2025-01-08 20:58:06 -0500

    Add utils.js file for common functions. Add generateRandonString. Add  quizzes-api.js to routes. Update server.js to require quizzes-api.js.

---
+++ routes/quizzes-api.js
@@ -0,0 +1,10 @@
+/**
+ * All routes for Quizzes Data are defined here
+ * Since this file is loaded in server.js into api/quizzes,
+ *   these routes are mounted onto /api/quizzes
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+const db = require('../db/connection');
