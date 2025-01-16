8021fd93e7f057947b8d09188ecee9a67f9867a1
ArcaneCipher <desjardins.jarp@gmail.com>
2025-01-16 02:02:23 -0500

    update homepage quizzes query. It's now sorted ascending

--- db/queries/select_homepage_quizzes.sql
+++ db/queries/select_homepage_quizzes.sql
@@ -1,10 +1,26 @@
-  -- Purpose: Fetches all public quizzes for the homepage.
-  SELECT 
-  category,     
-  JSON_AGG(
-    JSON_BUILD_OBJECT('title', title, 'quiz_url', quiz_url)
-  ) AS quizzes 
-  FROM quizzes
-  WHERE is_public = true 
-  GROUP BY category
-  ORDER BY category;
+-- Purpose: Fetches all public quizzes for the homepage.
+WITH all_categories AS (
+  SELECT enumlabel AS category
+  FROM pg_enum
+  WHERE enumtypid = (
+    SELECT oid
+    FROM pg_type
+    WHERE typname = 'category_enum'
+  )
+),
+quizzes_with_categories AS (
+  SELECT
+    c.category,
+    JSON_AGG(
+      JSON_BUILD_OBJECT('title', q.title, 'quiz_url', q.quiz_url)
+      ORDER BY q.title ASC
+    ) AS quizzes
+  FROM all_categories c
+  LEFT JOIN quizzes q
+  ON c.category = q.category::text -- Convert category_enum to text for comparison
+  AND q.is_public = true
+  GROUP BY c.category
+)
+SELECT *
+FROM quizzes_with_categories
+ORDER BY category ASC;
